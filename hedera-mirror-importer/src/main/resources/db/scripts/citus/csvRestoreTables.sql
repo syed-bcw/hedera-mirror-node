987621c9d81791ee7060fd66a52276f3eee7321c
Nana-EC <nana.essilfie-conduah@hedera.com>
2021-09-09 12:26:17 -0500

    Updated restore tables
    Signed-off-by: Nana-EC <nana.essilfie-conduah@hedera.com>

--- hedera-mirror-importer/src/main/resources/db/scripts/citus/csvRestoreTables.sql
+++ hedera-mirror-importer/src/main/resources/db/scripts/citus/csvRestoreTables.sql
@@ -2,19 +2,21 @@
 -- Restore Backup tables use efficient COPY process to CSV's
 -------------------
 
-\copy assessed_custom_fee (amount, collector_account_id, consensus_timestamp, token_id) from assessed_custom_fee.csv csv;
+--\copy account_balance (consensus_timestamp, balance, account_id) from account_balance.csv csv;
 
-\copy account_balance (consensus_timestamp, balance, account_id) from account_balance.csv csv;
-
-\copy account_balance_file (consensus_timestamp, count, load_start, load_end, file_hash, name, node_account_id, bytes) from account_balance_file.csv csv;
+\copy account_balance_file (bytes, consensus_timestamp, count, file_hash, load_start, load_end, name, node_account_id) from account_balance_file.csv csv;
 
 \copy address_book (start_consensus_timestamp, end_consensus_timestamp, file_id, node_count, file_data) from address_book.csv csv;
 
-\copy address_book_entry (id, consensus_timestamp, ip, port, memo, public_key, node_id, node_account_id, node_cert_hash) from address_book_entry.csv csv;
+\copy address_book_entry (consensus_timestamp, description, memo, public_key, node_id, node_account_id, node_cert_hash, stake) from address_book_entry.csv csv;
+
+\copy address_book_service_endpoint (consensus_timestamp, ip_address_v4, node_id, port) from address_book_service_endpoint.csv csv;
+
+\copy assessed_custom_fee (amount, collector_account_id, consensus_timestamp, token_id) from assessed_custom_fee.csv csv;
 
 \copy contract_result (function_parameters, gas_supplied, call_result, gas_used, consensus_timestamp) from contract_result.csv csv;
 
-\copy crypto_transfer (entity_id, consensus_timestamp, amount) from crypto_transfer.csv csv;
+--\copy crypto_transfer (entity_id, consensus_timestamp, amount) from crypto_transfer.csv csv;
 
 \copy custom_fee (amount, amount_denominator, collector_account_id, created_timestamp, denominating_token_id, maximum_amount, minimum_amount, token_id) from custom_fee.csv csv;
 
@@ -28,17 +30,17 @@
 
 \copy nft_transfer (consensus_timestamp, receiver_account_id, sender_account_id, serial_number, token_id) from nft_transfer.csv csv
 
-\copy record_file (name, load_start, load_end, hash, prev_hash, consensus_start, consensus_end, node_account_id, count, digest_algorithm, hapi_version_major, hapi_version_minor, hapi_version_patch, version, file_hash, bytes, index) from record_file.csv csv;
+--\copy record_file (bytes, consensus_start, consensus_end, count, digest_algorithm, file_hash, hapi_version_major, hapi_version_minor, hapi_version_patch, hash, "index", load_start, load_end, name, node_account_id, prev_hash, version) from record_file.csv csv;
 
-\copy schedule (consensus_timestamp, creator_account_id, executed_timestamp, payer_account_id, schedule_id, transaction_body) from schedule.csv csv;
+--\copy schedule (consensus_timestamp, creator_account_id, executed_timestamp, payer_account_id, schedule_id, transaction_body) from schedule.csv csv;
 
 \copy entity (auto_renew_account_id, auto_renew_period, created_timestamp, deleted, expiration_timestamp, id, key, memo, modified_timestamp, num, public_key, proxy_account_id, realm, shard, submit_key, type) from entity.csv csv;
 
-\copy token (token_id, created_timestamp, decimals, fee_schedule_key, fee_schedule_key_ed25519_hex, freeze_default, freeze_key, freeze_key_ed25519_hex, initial_supply, kyc_key, kyc_key_ed25519_hex, max_supply, modified_timestamp, name, supply_key, supply_key_ed25519_hex, supply_type, symbol, total_supply, treasury_account_id, type, wipe_key, wipe_key_ed25519_hex) from token.csv csv;
+--\copy token (token_id, created_timestamp, decimals, fee_schedule_key, fee_schedule_key_ed25519_hex, freeze_default, freeze_key, freeze_key_ed25519_hex, initial_supply, kyc_key, kyc_key_ed25519_hex, max_supply, modified_timestamp, name, supply_key, supply_key_ed25519_hex, supply_type, symbol, total_supply, treasury_account_id, type, wipe_key, wipe_key_ed25519_hex) from token.csv csv;
 
 \copy token_account (account_id, associated, created_timestamp, freeze_status, kyc_status, modified_timestamp, token_id) from token_account.csv csv;
 
-\copy token_balance (consensus_timestamp, account_id, balance, token_id) from token_balance.csv csv;
+--\copy token_balance (consensus_timestamp, account_id, balance, token_id) from token_balance.csv csv;
 
 \copy token_transfer (token_id, account_id, consensus_timestamp, amount) from token_transfer.csv csv;
 
